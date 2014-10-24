93250c63f4bb7049de0ddfabb52ec8488229b65c
Isaac Freeman <isaac@resolvedigital.co.nz>
2014-10-24 16:19:12 +1300

    Specs passing

--- pages/spec/features/refinery/admin/pages_spec.rb
+++ pages/spec/features/refinery/admin/pages_spec.rb
@@ -382,7 +382,7 @@ module Refinery
           it "shows locale flag for page" do
             p = ::Refinery::Page.by_slug('news').first
             within "#page_#{p.id}" do
-              expect(page).to have_css("img[src='/assets/refinery/icons/flags/en.png']")
+              expect(page).to have_css(".locale_icon.en")
             end
           end
 
@@ -460,8 +460,8 @@ module Refinery
             visit refinery.admin_pages_path
 
             within "#page_#{news_page.id}" do
-              expect(page).to have_css("img[src='/assets/refinery/icons/flags/en.png']")
-              expect(page).to have_css("img[src='/assets/refinery/icons/flags/ru.png']")
+              expect(page).to have_css(".locale_icon.en")
+              expect(page).to have_css(".locale_icon.ru")
             end
           end
 
@@ -528,7 +528,7 @@ module Refinery
 
           it "shows locale flag for page" do
             within "#page_#{ru_page_id}" do
-              expect(page).to have_css("img[src='/assets/refinery/icons/flags/ru.png']")
+              expect(page).to have_css(".locale_icon.ru")
             end
           end
 
